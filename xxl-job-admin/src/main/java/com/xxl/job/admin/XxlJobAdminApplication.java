889e11e31ace685eccb9aa83877988517aa62627
lubo <454331202@qq.com>
2021-06-23 18:00:19 +0800

    调整xxl-job-admin 部署方式

--- xxl-job-admin/src/main/java/com/xxl/job/admin/XxlJobAdminApplication.java
+++ xxl-job-admin/src/main/java/com/xxl/job/admin/XxlJobAdminApplication.java
@@ -2,15 +2,39 @@ package com.xxl.job.admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+//add dep
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.net.UnknownHostException;
+import java.net.InetAddress;
+
+import org.springframework.core.env.Environment;
 
 /**
  * @author xuxueli 2018-10-28 00:38:13
  */
+
 @SpringBootApplication
+
 public class XxlJobAdminApplication {
 
-	public static void main(String[] args) {
-        SpringApplication.run(XxlJobAdminApplication.class, args);
-	}
+    //	public static void main(String[] args) {
+//        SpringApplication.run(XxlJobAdminApplication.class, args);
+//	}
+    private static Logger logger = LoggerFactory.getLogger(XxlJobAdminApplication.class);
 
+    public static void main(String[] args) throws UnknownHostException {
+        Environment env = new SpringApplication(XxlJobAdminApplication.class).run(args).getEnvironment();
+        String envPort = env.getProperty("server.port");
+        String envContext = env.getProperty("server.servlet.context-path");
+        String context = envContext == null ? "" : envContext;
+        String path = envPort + "" + context;
+        String externalAPI = InetAddress.getLocalHost().getHostAddress();
+        logger.info(
+                "Access URLs:\n----------------------------------------------------------\n\t"
+                        + "External-URL: \thttp://{}:{}\n\t"
+                        + "web-URL: \thttp://127.0.0.1:{}\n\t----------------------------------------------------------",
+                externalAPI, path, path);
+    }
 }
